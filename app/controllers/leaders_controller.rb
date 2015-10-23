31c3e3a76658fe084417e53afe33313ee5db5945
Lyonsclay <lyonsclay@yahoo.com>
2015-10-23 02:28:22 -0400

    Create overlay on profile pic and style

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -6,12 +6,15 @@ class LeadersController < ApplicationController
 
   def create
     address = params[:voter_district][:address]
+
     @leaders = Rails.cache.fetch("leaders/#{address}", expires_in: 2.days) do
       RepsGetter.new(address).get
     end
+
     render action: "index"
   end
 
   def index
+    byebug
   end
 end
